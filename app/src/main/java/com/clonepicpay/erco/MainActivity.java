74beac913a3fd7d7938898c8be7eb5a02c858607
João Victor <56576465+JoaoVictor-UFC@users.noreply.github.com>
2021-01-17 22:52:08 -0300

    Creation of th first screens
    Tela principal e Tela Login criadas

--- app/src/main/java/com/clonepicpay/erco/MainActivity.java
+++ app/src/main/java/com/clonepicpay/erco/MainActivity.java
@@ -2,13 +2,35 @@ package com.clonepicpay.erco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoEntrar = (Button) findViewById(R.id.button2);
+
+        botaoEntrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                botaoEntrarActivity();
+            }
+        });
+    }
+
+    private void botaoEntrarActivity() {
+
+        startActivity(new Intent(MainActivity.this, TelaEntrar.class));
     }
 }
---
+++ app/src/main/java/com/clonepicpay/erco/TelaEntrar.java
@@ -0,0 +1,13 @@
+package com.clonepicpay.erco;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaEntrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_entrar);
+    }
+}
