f7a7b14d7bb2fff1e07307df71b92e5fb6f7f6ee
João Victor <56576465+JoaoVictor-UFC@users.noreply.github.com>
2021-01-27 23:36:47 -0300

    New window register
    nova tela para cadastro de clientes

--- app/src/main/java/com/clonepicpay/erco/MainActivity.java
+++ app/src/main/java/com/clonepicpay/erco/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
+import com.clonepicpay.erco.register.Register001;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button botaoEntrar;
+    private Button botaoCadatrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         botaoEntrar = (Button) findViewById(R.id.button2);
+        botaoCadatrar = (Button) findViewById(R.id.button1);
 
         botaoEntrar.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
 
                 botaoEntrarActivity();
+            }
+        });
+
+        botaoCadatrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                botaoCadatrarActivity();
             }
         });
     }
@@ -33,4 +42,11 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(new Intent(MainActivity.this, TelaEntrar.class));
     }
+
+    private void botaoCadatrarActivity() {
+
+        startActivity(new Intent(MainActivity.this, Register001.class));
+    }
+
+
 }
---
+++ app/src/main/java/com/clonepicpay/erco/register/Register002.java
@@ -0,0 +1,16 @@
+package com.clonepicpay.erco.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.clonepicpay.erco.R;
+
+public class Register002 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register002);
+    }
+}
